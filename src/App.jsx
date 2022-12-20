2b7d628d781237c5ba66dfdde842b3f5fdd9e2a3
ekaone <ekaone3033@gmail.com>
2022-12-20 14:15:29 +0700

    init

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { Box } from "@chakra-ui/react";
+
+function App() {
+  return (
+    <>
+      <Box bg="tomato" w="100%" p={4} color="white">
+        Vite React
+      </Box>
+    </>
+  );
+}
+
+export default App;
